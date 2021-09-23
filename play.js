01d9bfaf15527458f3c3f0925a480b4f498c84e7
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-09-23 07:08:34 -0400

    Added an exit command (ctrl C)

--- play.js
+++ play.js
@@ -3,3 +3,24 @@ const connect = require("./client");
 console.log("Connecting ...");
 connect();
 
+//listen for keyboard input
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+//keybinds
+const handleUserInput = function() {
+  const stdin = process.stdin;
+  stdin.on("data", (key) => {
+    if (key === '\u0003'/*Ctrl C*/) {
+      process.exit();
+    }
+  });
+};
+
+setupInput();
